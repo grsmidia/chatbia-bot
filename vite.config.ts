ec05c9875d61f62798cf96f7e4630b9f83655078
sarsicha <atendimento@grsmidia.com.br>
2026-01-05 17:13:15 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  },
+  build: {
+    lib: {
+      entry: 'index.tsx',
+      name: 'BiaChat',
+      fileName: () => 'assets/bia-chat.js',
+      formats: ['es']
+    },
+    rollupOptions: {
+      output: {
+        extend: true,
+      }
+    }
   }
 });
